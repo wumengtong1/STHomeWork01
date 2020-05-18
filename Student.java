4e79f8823ee15ec305943db2eac047373ee69508
wumengtong1 <50296051+wumengtong1@users.noreply.github.com>
2020-05-19 00:54:34 +0800

    first commit

--- Student.java
+++ Student.java
@@ -1,64 +1,23 @@
-package homework;
+package wumengtong;
+public class Student {
+    private int ID;
+    private String name;
+    private String birDate;
+    private boolean gender;
 
-public class Student implements Comparable<Object>{
-	private int ID=100000000;
-	private String name;
-	private String birDay;
-	private Boolean gender;
+    public int getID() {return ID; }
 
-//	public Student(int ID,String name,String birDay,Boolean gender) {
-//		this.ID=ID;
-//		this.name = name;
-//		this.birDay = birDay;
-//		this.gender=gender;
-//	}
-	
-	public void setID(int id) {
-		ID=id;
-	}
-	
-	public int getID() {
-		return ID;
-	}
-	
-	public void setName(String name) {
-		this.name=name;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setBirDay(String birday) {
-		this.birDay=birday;
-	}
-	
-	public String getBirDay() {
-		return birDay;
-	}
-	
-	public void setGender(Boolean gender) {
-		this.gender=gender;
-	}
-	
-	public Boolean getGender() {
-		return gender;
-	}
-	public int compareTo(Object o) {
-		if (o instanceof Student) {
-			Student s = (Student) o;
-			if (this.ID > s.ID) {
-				return 1;
-			} else if (this.ID == s.ID) {
-				return 0;
-			} else {
-				return -1;
-			}
-		}
-		return 0;
+    public void setID(int ID) {this.ID = ID; }
 
-	}
-	public String toString() {
-		return "Student [ID=" + ID + ", name=" + name +",birDay="+ birDay + ",gender=" + gender + "]";
-	}
+    public String getBirDate() {return birDate;}
+
+    public void setBirDate(String birDate) {this.birDate = birDate;}
+
+    public boolean isGender() {return gender;}
+
+    public void setGender(boolean gender) {this.gender = gender;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name; }
 }
