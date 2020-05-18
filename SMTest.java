953c83b6651d2f3dd03a3fbbbb351013205726a3
wumengtong1 <50296051+wumengtong1@users.noreply.github.com>
2020-05-19 00:57:03 +0800

    first commit

--- SMTest.java
+++ SMTest.java
@@ -3,11 +3,10 @@ package wumengtong;
 public class SMTest {
     public static void main(String[] args) {
         StudentManager studentManager = new StudentManager();
-        StudentManager.addStudent(1,"吴孟桐","1999.7",true);
+        StudentManager.addStudent(1,"吴孟桐","1999.7.18",true);
         StudentManager.seek("吴孟桐");
         StudentManager. change("吴孟桐");
         StudentManager.printAll();
         StudentManager.tuichu();
     }
 }
-
